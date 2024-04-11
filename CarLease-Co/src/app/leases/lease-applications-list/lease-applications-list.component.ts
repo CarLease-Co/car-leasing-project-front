18bd06d2196e14573fc25c2c23754551de1030a3
Urte Nistirenko <urte.nistirenko@gmail.com>
2024-04-11 21:33:55 +0300

    generate lease-applications-list component

---
+++ CarLease-Co/src/app/leases/lease-applications-list/lease-applications-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lease-applications-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './lease-applications-list.component.html',
+  styleUrl: './lease-applications-list.component.scss'
+})
+export class LeaseApplicationsListComponent {
+
+}
