8f0d1dc02f9199cbc728a0f2b7d87e3302ca79d2
EgleeB <117934410+EgleeB@users.noreply.github.com>
2024-04-12 16:04:36 +0300

    created a form without sliders

--- CarLease-Co/src/app/leases/lease-application-form/lease-application-form.component.ts
+++ CarLease-Co/src/app/leases/lease-application-form/lease-application-form.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import {
+  MatFormFieldControl,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatOption } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
   selector: 'app-lease-application-form',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatOption,
+    MatSelectModule,
+    MatSliderModule,
+  ],
   templateUrl: './lease-application-form.component.html',
-  styleUrl: './lease-application-form.component.scss'
+  styleUrl: './lease-application-form.component.scss',
 })
-export class LeaseApplicationFormComponent {
-
-}
+export class LeaseApplicationFormComponent {}
