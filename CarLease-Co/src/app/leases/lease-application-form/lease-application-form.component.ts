0bebd36871e8ba7d2d06481b9ef4dc17da002da5
EgleeB <117934410+EgleeB@users.noreply.github.com>
2024-04-12 14:48:38 +0300

    Created a lease-application-form component

---
+++ CarLease-Co/src/app/leases/lease-application-form/lease-application-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lease-application-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './lease-application-form.component.html',
+  styleUrl: './lease-application-form.component.scss'
+})
+export class LeaseApplicationFormComponent {
+
+}
