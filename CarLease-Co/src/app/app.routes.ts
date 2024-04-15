647261dc47fd34a244bb716f93af72db57533432
EgleeB <117934410+EgleeB@users.noreply.github.com>
2024-04-15 17:48:59 +0300

    added a route to a form

--- CarLease-Co/src/app/app.routes.ts
+++ CarLease-Co/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LeaseApplicationsListComponent } from './leases/lease-applications-list/lease-applications-list.component';
+import { LeaseApplicationFormComponent } from './leases/lease-application-form/lease-application-form.component';
 
 export const routes: Routes = [
   { path: 'applications', component: LeaseApplicationsListComponent },
+  { path: 'new-application', component: LeaseApplicationFormComponent },
 ];
