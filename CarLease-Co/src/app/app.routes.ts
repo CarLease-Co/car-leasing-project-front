1ff55b8cf25bba8b8f9034568c7de7f5d7ba8c5d
EgleeB <117934410+EgleeB@users.noreply.github.com>
2024-04-15 18:13:51 +0300

    Added working routes

--- CarLease-Co/src/app/app.routes.ts
+++ CarLease-Co/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LeaseApplicationsListComponent } from './leases/lease-applications-list/lease-applications-list.component';
 import { LeaseApplicationFormComponent } from './leases/lease-application-form/lease-application-form.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   { path: 'applications', component: LeaseApplicationsListComponent },
