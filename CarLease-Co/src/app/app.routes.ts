6a08b4484776cfb45b4fbbdd130f95c2a7898def
Urte Nistirenko <urte.nistirenko@gmail.com>
2024-04-18 13:01:40 +0300

    add login form

--- CarLease-Co/src/app/app.routes.ts
+++ CarLease-Co/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LeaseApplicationsListComponent } from './leases/lease-applications-list/lease-applications-list.component';
 import { LeaseDetailsComponent } from './leases/lease-details/lease-details.component';
 import { LeaseApplicationFormComponent } from './leases/lease-application-form/lease-application-form.component';
-import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login/login-form/login-form.component';
 
 export const routes: Routes = [
   { path: 'applications', component: LeaseApplicationsListComponent },
   { path: 'application-details/:id', component: LeaseDetailsComponent },
   { path: 'new-application', component: LeaseApplicationFormComponent },
+  { path: 'login', component: LoginFormComponent },
 ];
--- CarLease-Co/src/app/login/login-form/login-form.component.ts
+++ CarLease-Co/src/app/login/login-form/login-form.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.scss'
+  styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
-
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.min(3)]),
+    password: new FormControl('', Validators.required),
+  });
+  private readonly router = inject(Router);
+  login() {
+    console.log('logged in');
+    this.router.navigate(['applications']);
+  }
 }
