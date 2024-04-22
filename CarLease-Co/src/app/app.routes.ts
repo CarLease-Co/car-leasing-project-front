95e44882d49d7039d5b06f110902daf78fcd090d
Urte Nistirenko <urte.nistirenko@gmail.com>
2024-04-22 12:27:12 +0300

    create busadmin view

--- CarLease-Co/src/app/app.routes.ts
+++ CarLease-Co/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LeaseApplicationsListComponent } from './leases/lease-applications-list
 import { LeaseDetailsComponent } from './leases/lease-details/lease-details.component';
 import { LeaseApplicationFormComponent } from './leases/lease-application-form/lease-application-form.component';
 import { AppComponent } from './app.component';
+import { AutosuggestorFormComponent } from './business-admin-view/autosuggestor-form/autosuggestor-form.component';
 
 export const routes: Routes = [
   { path: 'applications', component: LeaseApplicationsListComponent },
   { path: 'application-details/:id', component: LeaseDetailsComponent },
   { path: 'new-application', component: LeaseApplicationFormComponent },
+  { path: 'autosuggestor-form', component: AutosuggestorFormComponent },
 ];
---
+++ CarLease-Co/src/app/business-admin-view/autosuggestor-form/autosuggestor-form.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-autosuggestor-form',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatSliderModule,
+    MatButtonModule,
+  ],
+  templateUrl: './autosuggestor-form.component.html',
+  styleUrl: './autosuggestor-form.component.scss',
+})
+export class AutosuggestorFormComponent {
+  autosuggestorForm = new FormGroup({
+    monthlyIncome: new FormControl('', [
+      Validators.required,
+      Validators.min(1),
+    ]),
+    monthlyObligations: new FormControl('', [
+      Validators.required,
+      Validators.min(1),
+    ]),
+    carMake: new FormControl('', Validators.required),
+    carModel: new FormControl('', Validators.required),
+    carYear: new FormControl(1925, [
+      Validators.required,
+      Validators.min(1925),
+      Validators.max(2024),
+    ]),
+    duration: new FormControl(3, [
+      Validators.required,
+      Validators.min(3),
+      Validators.max(68),
+    ]),
+    leasingAmount: new FormControl('', [
+      Validators.required,
+      Validators.min(1),
+    ]),
+    explanation: new FormControl(''),
+  });
+  onSubmit() {}
+}
