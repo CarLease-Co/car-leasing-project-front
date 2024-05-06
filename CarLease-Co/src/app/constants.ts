88dcd0a02449968b536ec62d1274ca3d4fcb7d16
Urte Nistirenko <urte.nistirenko@gmail.com>
2024-05-06 10:45:22 +0300

    change base_url

--- CarLease-Co/src/app/constants.ts
+++ CarLease-Co/src/app/constants.ts
@@ -1,5 +1,5 @@
 export const LOGIN_RESPONSE_KEY = 'loginResponse';
-export const BASE_URL = 'https://car-leasing-project-back-sandbox.onrender.com';
+export const BASE_URL = 'https://car-leasing-project-back-1.onrender.com';
 export const APPLICATIONS_PATH = '/api/v1/applications';
 export const CAR_PATH = '/api/v1/cars';
 export const CAR_PATCH_PATH = '/api/v1/cars/';
